d1e89a856e1e8bcd4074247b61419c087b76f5ea
Daniel M. Drucker <ddrucker@mclean.harvard.edu>
2019-09-13 12:09:18 -0400

    whole screen

--- wxpaper.py
+++ wxpaper.py
@@ -2,50 +2,116 @@
 
 from darksky.api import DarkSky, DarkSkyAsync
 from darksky.types import languages, units, weather
-API_KEY = open('darksky-secret').readline().rstrip()
-darksky = DarkSky(API_KEY)
-lat, lon = 42.417821,-71.177747
+from sh import particle
 
-forecast = darksky.get_forecast(lat,lon,
-                                exclude=[weather.MINUTELY, weather.ALERTS])
+particle_id = '3eink'
+API_KEY = open("darksky-secret").readline().rstrip()
+darksky = DarkSky(API_KEY)
+lat, lon = 42.417821, -71.177747
+
+forecast = darksky.get_forecast(lat, lon, exclude=[weather.MINUTELY, weather.ALERTS])
 
 now = forecast.currently
 today = forecast.daily.data[0]
 
+
 def clamp(n, minn, maxn):
     return max(min(maxn, n), minn)
+
 
 def two_dig(n, HILO=True):
     if HILO:
         if n >= 100:
-            return 'HI'
+            return "HI"
         if n < 0:
-            return 'LO'
-    return '{:02d}'.format(int(clamp(n,0,99)))
+            return "LO"
+    return "{:02d}".format(int(clamp(n, 0, 99)))
+
 
 def uv_one_dig(n):
     if n > 9:
-        return '!'
-    return '{:01d}'.format(int(n))
+        return "X"
+    return "{:01d}".format(int(n))
 
-iconmap = {'clear-day': 'SUN',
-          'clear-night': 'NIGHT',
-          'cloudy': 'CLOUD',
-          'partly-cloudy-day': 'CLDAY',
-          'partly-cloudly-night': 'CLDNT'}
+
+iconmap = {
+    "clear-day": "SUN",
+    "clear-night": "NIGHT",
+    "cloudy": "CLOUD",
+    "partly-cloudy-day": "CLDAY",
+    "partly-cloudly-night": "CLDNT",
+}
+
 
 def icon(iconsize, condition):
-    return f'{iconsize}' + iconmap.get(condition, condition.upper()) + '.PNG'
+    return f"{iconsize}" + iconmap.get(condition, condition.upper()) + ".BMP"
+
 
 # current, high, low, current-icon, UV, worst-icon, precipprob, summary
-payload = '|'.join(
-        [two_dig(now.apparent_temperature),
- two_dig(today.apparent_temperature_high),
- two_dig(today.apparent_temperature_low),
- icon(3, now.icon),
- uv_one_dig(today.uv_index),
- icon(2, today.icon),
- two_dig(100 * today.precip_probability, HILO=False),
- today.summary
-])
+payload = "|".join(
+    [
+        two_dig(now.apparent_temperature),
+        two_dig(today.apparent_temperature_high),
+        two_dig(today.apparent_temperature_low),
+        icon(3, now.icon),
+        uv_one_dig(today.uv_index),
+        icon(3, today.icon),
+        two_dig(100 * today.precip_probability, HILO=False),
+        today.summary,
+    ]
+)
 print(payload)
+
+
+
+def paper_image(bitmap, x, y):
+    particle('function', 'call', particle_id, 'img', '|'.join((bitmap, str(x), str(y))))
+
+def paper_bignum(n, x, y):
+    paper_image(f'300_{n}.BMP', x, y)
+
+def paper_smallnum(n, x, y):
+    paper_image(f'150_{n}.BMP', x, y)
+
+def paper_text(text, x, y):
+    particle('function', 'call', particle_id, 'text', '|'.join((text, str(x), str(y))))
+
+def paper_clear():
+    particle('function', 'call', particle_id, 'clear')
+
+def paper_update():
+    particle('function', 'call', particle_id, 'update')
+
+
+
+paper_clear()
+# temp_now      20,20   160,20
+temp_now = two_dig(now.apparent_temperature)
+paper_bignum(temp_now[0], 20, 20)
+paper_bignum(temp_now[1], 160, 20)
+
+# temp_hi       310,20  380,20
+temp_hi = two_dig(today.apparent_temperature_high)
+paper_smallnum(temp_hi[0], 310, 20)
+paper_smallnum(temp_hi[1], 380, 20)
+
+# temp_low      310,170 380,170
+temp_low = two_dig(today.apparent_temperature_low)
+paper_smallnum(temp_low[0], 310, 170)
+paper_smallnum(temp_low[1], 380, 170)
+
+# icon_now      500,30
+paper_image(icon(3, now.icon), 500, 30)
+
+# icon_today    500,290
+paper_image(icon(3, today.icon), 500, 290)
+
+# icon_shades   20,470
+paper_image('UV.BMP', 20, 470)
+
+# uv            80,320
+paper_smallnum(uv_one_dig(today.uv_index), 80, 320)
+
+paper_text(today.summary, 20, 570)
+
+paper_update()
