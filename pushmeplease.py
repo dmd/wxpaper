d95e98cca46dbb49c8d6a7f7b64d07cb14b39e59
Daniel M. Drucker <dmd@3e.org>
2019-09-15 09:46:14 -0400

    use attic.local

--- pushmeplease.py
+++ pushmeplease.py
@@ -18,13 +18,13 @@ class S(BaseHTTPRequestHandler):
                 wxpaper.do_update()
                 self.wfile.write(b"ok\n")
             except:
-                self.wfile.write(b"uh oh\n")
+                self.wfile.write(b"running wxpaper failed\n")
         else:
             print("got invalid request")
             self.wfile.write(b"go away\n")
 
 
-def run(server_class=HTTPServer, handler_class=S, port=8099):
+def run(server_class=HTTPServer, handler_class=S, port=33223):
     server_address = ("", port)
     httpd = server_class(server_address, handler_class)
     print("Starting httpd...")
@@ -32,9 +32,4 @@ def run(server_class=HTTPServer, handler_class=S, port=8099):
 
 
 if __name__ == "__main__":
-    from sys import argv
-
-    if len(argv) == 2:
-        run(port=int(argv[1]))
-    else:
-        run()
+    run()
